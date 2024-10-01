df3083d5fb0c6e2c0d3f61a181d6ce0d1ca318d3
BuseNurAydn <83844222+BuseNurAydn@users.noreply.github.com>
2024-10-01 15:09:27 +0300

    Footer yapıldı

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,17 @@
-import React,{useEffect,useState} from 'react'
-import FooterMenuItem from  '../api/FooterMenu.json'
+import React, { useEffect, useState } from 'react'
+import FooterMenuItem from '../api/FooterMenu.json'
 import FooterMenu from '../components/ui/FooterMenu'
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa'
+import { BiGlobe } from 'react-icons/bi'
 
 
 export default function Footer() {
   const [menus, setMenu] = useState([]);
 
-  useEffect(()=>{
-       //İSTEK
-       setMenu(FooterMenuItem);
-  },[]);
+  useEffect(() => {
+    //İSTEK
+    setMenu(FooterMenuItem);
+  }, []);
 
   return (
     <div className='bg-white'>
@@ -29,12 +31,34 @@ export default function Footer() {
               </a>
             </nav>
           </section>
-           {menus.map((menu, index) => (
+          {menus.map((menu, index) => (
             <FooterMenu key={index} title={menu.title} items={menu.items} />
           ))}
         </div>
+        <div className="flex justify-between items-center border-t border-gray-200 mt-6 py-8">
+          <div className="text-xs text-gray-700 flex gap-x-8 px-16"> &copy; 2021 Getir
+            <a href="#" className="text-brand-color relative before:w-[3px] before:h-[3px] before:bg-black before:absolute before:-left-4 before:top-1/2 before:-translate-y-1/2 before:rounded-full">
+              Bilgi Toplumu Hizmetleri
+            </a>
+          </div>
+          <nav className="flex gap-x-3 px-16">
+            <a href="#" className="w-8 h-8 rounded-lg text-gray-500 transition-colors hover:bg-brand-color hover:bg-opacity-10 hover:text-primary-brand-color flex items-center justify-center">
+              <FaFacebook size={21} />
+            </a>
+            <a href="#" className="w-8 h-8 rounded-lg text-gray-500 transition-colors hover:bg-brand-color hover:bg-opacity-10 hover:text-brand-color flex items-center justify-center">
+              <FaTwitter size={21} />
+            </a>
+            <a href="#" className="w-8 h-8 rounded-lg text-gray-500 transition-colors hover:bg-brand-color hover:bg-opacity-10 hover:text-brand-color flex items-center justify-center">
+              <FaInstagram size={21} />
+            </a>
+            <a href="#" className="h-8 transition-colors hover:bg-brand-color hover:bg-opacity-10 hover:text-brand-color hover:border-transparent flex items-center px-2 text-sm gap-x-2 rounded text-gray-500 border border-gray-100">
+              <BiGlobe size={18} />
+              Türkçe (TR)
+            </a>
+          </nav>
+        </div>
       </div>
-
     </div>
+
   )
 }
