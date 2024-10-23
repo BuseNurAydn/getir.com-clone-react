2d88f4b91ab29251329d434914ec0a4c73ee039d
BuseNurAydn <83844222+BuseNurAydn@users.noreply.github.com>
2024-10-23 11:44:49 +0300

    getirçarşı yapılıyor

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import GetirSu from './pages/GetirSu/HomeSu';
 import GetirBuyuk from './pages/GetirBuyuk/HomeBuyuk';
+import GetirCarsi from './pages/GetirCarsi/HomeCarsi';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" exact element={<Home/>}/>
         <Route path='/buyuk/' element={<GetirBuyuk/>} />
         <Route path="/su/" element={<GetirSu/>}  />
+        <Route path='/carsi/' element={<GetirCarsi/>} />
       </Routes>
     </Router>
   );
--- src/ui/Category.js
+++ src/ui/Category.js
@@ -1,7 +1,7 @@
 export default function Category({ category: {id, title, image} }) {
     return (
         <a href="#" className="flex flex-col group items-center gap-y-2 text-center py-4 transition hover:bg-purple-100 rounded">
-            <img src={image} alt={title} className="w-12 h-12 rounded border border-gray-200" />
+            <img src={image} alt={title} className="w-12 h-12 rounded-lg border border-gray-200" />
             <span className="text-sm font-semibold text-gray-700 whitespace-nowrap group-hover:text-brand-color tracking-tight tracking-tight">{title}</span>
         </a>
     )
