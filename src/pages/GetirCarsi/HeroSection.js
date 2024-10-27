a29db08d409af2d25b2d76933067dc4bf10fe694
BuseNurAydn <83844222+BuseNurAydn@users.noreply.github.com>
2024-10-27 18:01:39 +0300

    getirçarşı tamamlandı

--- src/pages/GetirCarsi/HeroSection.js
+++ src/pages/GetirCarsi/HeroSection.js
@@ -1,6 +1,8 @@
 
 import React, { useState } from "react";
 import ReactFlagsSelect from "react-flags-select";
+import searchIcon from '../../img/svg/search.svg';
+import crossIcon from '../../img/svg/crosshair.svg'
 
 const HeroSection = () => {
 
@@ -19,12 +21,33 @@ const HeroSection = () => {
         <div className='bg-carsi h-[440px] w-full flex justify-end pr-8'>
             <div className=" flex items-center justify-between mr-16 ">
                 <div className='md:w-[400px] rounded-lg bg-gray-color p-6'>
-                    <label className='flex-1 relative block cursor-pointer mb-4'>
-                        <input required className="h-14 px-5 border-2 border-gray-300 rounded w-full text-sm focus:pt-2  transition-colors hover:border-brand-color outline-none peer" />
-                        <span className="absolute top-0 left-0 h-full flex items-center px-7 text-sm transition-all text-gray-500 peer-focus:h-8 peer-focus:text-brand-color peer-focus:text-xs peer-valid:h-8 peer-valid:text-brand-color peer-valid:text-xs" >Sokağını veya posta kodunu bul</span>
-                    </label>
-                    <div className="border-b-2 divide-slate-400 w-full"></div>
-                    <h3 className='text-brand-color text-center font-semibold m-2'>Giriş yap veya kayıt ol</h3>
+                    <div className='flex items-center mb-4'>
+                        <label className='flex-1 relative block cursor-pointer'>
+                            <img
+                                src={searchIcon}
+                                alt="Search Icon"
+                                className="absolute left-5 top-1/2 transform -translate-y-1/2 w-4 h-4"
+                            />
+                            <input
+                                placeholder="Sokağını veya posta kodunu arat"
+                                required
+                                className="h-14 px-12 border-2 border-gray-300 rounded w-full text-sm transition-colors hover:border-brand-color outline-none peer"
+                            />
+                        </label>
+                        <button
+                            className="ml-44 absolute px-4 py-2 bg-gray-color rounded-lg text-sm font-semibold hover:bg-brand-dark transition-colors flex items-center space-x-2">
+                            <img
+                                src={crossIcon}
+                                alt="Button Icon"
+                                className="w-4 h-4"
+                            />
+                            <span className="text-brand-color">Konumumu bul</span>
+                        </button>
+                    </div>
+                    <div className=" relative flex items-center border-b-2 my-8 divide-slate-400 w-full">
+                        <span className="absolute bg-gray-color ml-36 text-sm px-4 font-semibold text-slate-500">veya</span>
+                    </div>
+                    <h3 className='text-brand-color text-center font-semibold m-4'>Giriş yap veya kayıt ol</h3>
                     <div className="flex mt-4">
                         <ReactFlagsSelect
                             countries={Object.keys(phones)}
