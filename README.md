09c7a7ccfc1fdc02940a9ec7fd08fb17d3606c06
BuseNurAydn <83844222+BuseNurAydn@users.noreply.github.com>
2024-11-16 13:49:56 +0300

    düzenlemeler ve eklemeler yapıldı

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Repo where getir.com website is coded with React.js and Tailwind CSS
 ![Proje görsel 1](/src/assets/png/getirgörsel1.png)
 ![Proje görsel 2](/src/assets/png/getirgörsel2.png)
 ![Proje görsel 3](/src/assets/png/getirgörsel3.png)
+![Proje görsel 4](/src/assets/png/getirgörsel4.png)
 
 
 
--- src/pages/GetirYemek/Business.js
+++ src/pages/GetirYemek/Business.js
@@ -8,7 +8,7 @@ const Business = () => {
         <h2 className='font-bold text-brand-color tracking-normal text-lg md:text-3xl leading-9'>Restoran sahibi misiniz?</h2>
         <span className='font-semibold text-xs md:text-base text-graytwo-color'>GetirYemek restoran iş ortaklarından biri olun, işinizi daha az maliyetle büyütün, müşteri memnuniyetini artırın.</span>
       </div>
-        <div className='flex justify-end md:mr-8 self-start md:self-center absolute md:relative mt-24 ml-4 md:ml-0 md:mt-0'>
+        <div className='flex justify-end md:mr-8 self-start md:self-center absolute md:relative mt-28 ml-4 md:ml-0 md:mt-0'>
           <button className='rounded px-8 py-3 md:py-4 bg-gradient-custom text-brand-color sm:text-sm font-semibold leading-4'>Hemen Başvur</button>
         </div>
       </div>
